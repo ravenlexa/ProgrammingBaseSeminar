85dc57b47d1c8c84bcc03853db849bf5afd98494
Aleksey <raven.lexa@yandex.ru>
2022-10-13 22:09:39 +0300

    1

--- Seminar/ConsoleApp_1/Program.cs
+++ Seminar/ConsoleApp_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrNumber = number * number;
+
+Console.WriteLine($"Квадрат вашего числа = {sqrNumber}");
+
+
+
+

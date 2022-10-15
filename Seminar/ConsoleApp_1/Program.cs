087d70376f5a3e67dd6ab8fdfcb4b058c2f68c29
Aleksey <raven.lexa@yandex.ru>
2022-10-15 21:39:41 +0300

    Сравнение

--- Seminar/ConsoleApp_1/Program.cs
+++ Seminar/ConsoleApp_1/Program.cs
@@ -1,10 +1,24 @@
-﻿Console.Write("Введите номер дня недели: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-if (numberA == 1)
-{
-    Console.WriteLine("понедельник");
-}
+﻿//
 
+Console.WriteLine("Давайте узнаем какое из двух чисел больше: ");
+Console.Write("Введите 1 число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB)
+{
+    Console.WriteLine("они равны");    
+}
+if (numberA > numberB)
+{
+    Console.Write("max ");
+    Console.WriteLine(numberA);
+}
+if (numberA < numberB)
+{   
+    Console.Write("max ");
+    Console.WriteLine(numberB);
+}
 
 
 

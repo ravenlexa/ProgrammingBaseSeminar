58a589c57ab229dfbb27b5ed8b6bf9fdcaae4497
Aleksey <raven.lexa@yandex.ru>
2022-10-22 13:15:42 +0300

    SEMINAR

---
+++ Seminar3/ConsoleApp_17/Program.cs
@@ -0,0 +1,37 @@
+﻿// <Возвращаемый тип> <Наименование метода>(переменные) {тело метода} 
+// Получить номер четверти на плоскости
+int GetPointArea(int coordX, int coordY)
+{
+    int numberArea = 0;
+    if (coordX > 0 && coordY > 0)
+    {
+        numberArea = 1;
+    }
+
+    if (coordX < 0 && coordY > 0)
+    {
+        numberArea = 2;
+    }
+
+    if (coordX < 0 && coordY < 0)
+    {
+        numberArea = 3;
+    }
+
+    if (coordX > 0 && coordY < 0)
+    {
+        numberArea = 4;
+    }
+    return numberArea;
+}
+
+System.Console.WriteLine( "Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine( "Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int numberArea = GetPointArea(x, y);
+System.Console.WriteLine(numberArea);
+
+

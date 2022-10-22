7ee4e4bd129c50751063abf097233978e2d3b87d
Aleksey <raven.lexa@yandex.ru>
2022-10-22 14:48:36 +0300

    ConsoleApp_7

---
+++ Seminar1/ConsoleApp_7/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+   и на выходе показывает последнюю цифру этого числа.*/
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int ostatok = number % 10;
+
+if (number > 999 || number < 100)
+{
+    System.Console.WriteLine("Это не трехначное число");
+}
+else
+{
+    System.Console.WriteLine($"Последняя цифра {ostatok}");
+}

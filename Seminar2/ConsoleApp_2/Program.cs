6dffb1ef3cf716c63628756af2f75246e56ef3e8
Aleksey <raven.lexa@yandex.ru>
2022-10-22 13:14:59 +0300

    SEMINAR

--- Seminar2/ConsoleApp_2/Program.cs
+++ Seminar2/ConsoleApp_2/Program.cs
@@ -1,6 +1,24 @@
 ﻿
 int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number  % 10;
 
 Console.WriteLine(number);
+
+if (a1 > a2)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {a1}");
+}
+
+if (a2 > a1)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {a2}");
+}
+
+if (a2 == a1)
+{
+    Console.WriteLine("Цифры в числе равны");
+}
+
 Console.ReadKey();
 

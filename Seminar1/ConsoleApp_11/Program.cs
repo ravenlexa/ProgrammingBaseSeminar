97d6f9162b4bdaba29c7eb697150896c00754198
Aleksey <raven.lexa@yandex.ru>
2022-10-22 15:55:24 +0300

    ConsoleApp_11

---
+++ Seminar1/ConsoleApp_11/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Напишите программу, которая выводит случайное трёхзначное число 
+  и удаляет вторую цифру этого числа.*/
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+int a1 = number / 100;
+int a2 = number % 10;
+int result = a1 * 10 + a2;
+System.Console.WriteLine(result);

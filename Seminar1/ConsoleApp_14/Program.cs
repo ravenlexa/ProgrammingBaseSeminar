2f0e0829a6b6653ed247c2caa3bb2e91969b3368
Aleksey <raven.lexa@yandex.ru>
2022-10-22 17:16:53 +0300

    ConsoleApp_14

--- Seminar1/ConsoleApp_14/Program.cs
+++ Seminar1/ConsoleApp_14/Program.cs
@@ -1,4 +1,6 @@
-﻿System.Console.WriteLine("Введите число ");
+﻿/*Напишите программу, которая принимает на вход число и проверяет, 
+  кратно ли оно одновременно 7 и 23.*/
+System.Console.WriteLine("Введите число ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 if (a % 7 == 0 && a % 23 == 0)

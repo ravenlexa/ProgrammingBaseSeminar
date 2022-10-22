050902ecd5498bd2a050691feb0246b56ec35c46
Aleksey <raven.lexa@yandex.ru>
2022-10-22 17:12:05 +0300

    ConsoleApp_14

---
+++ Seminar1/ConsoleApp_14/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    System.Console.WriteLine("Кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine("Не кратно");
+}

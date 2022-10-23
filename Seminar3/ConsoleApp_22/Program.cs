cefadfd1736c66790e32552f7b67b362693b5651
Aleksey <raven.lexa@yandex.ru>
2022-10-23 22:03:35 +0300

    ConsoleApp_22

---
+++ Seminar3/ConsoleApp_22/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу квадратов чисел от 1 до N.*/
+
+//Квадрат чисел от 1 до n
+void PrintSquaeresToConsole(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        System.Console.WriteLine(i * i);
+    }
+}
+System.Console.Write( "Введите N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintSquaeresToConsole(number);

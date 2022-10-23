5a271ddfe9c60adf62c209c714f1ab8fad6a629f
Aleksey <raven.lexa@yandex.ru>
2022-10-23 21:01:42 +0300

    ConsoleApp_18

--- Seminar3/ConsoleApp_18/Program.cs
+++ Seminar3/ConsoleApp_18/Program.cs
@@ -1,2 +1,42 @@
-﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-              
+﻿/*Задача 18: Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+// Принимаем номер четверти и выводим строку с диапозоном.
+string DiapozonCoords(int numberArea)
+{
+    string diapozon = "";
+    if (numberArea < 1 || numberArea > 4)
+    {
+        diapozon = "Нет такой четверти";
+    }
+    else
+    {
+        if (numberArea == 1)
+        {
+            diapozon = "coordX > 0 && coordY > 0";
+        }
+
+        if (numberArea == 2)
+        {
+            diapozon = "coordX > 0 && coordY < 0";
+        }
+
+        if (numberArea == 3)
+        {
+            diapozon = "coordX < 0 && coordY < 0";
+        }
+
+        if (numberArea == 4)
+        {
+            diapozon = "coordX < 0 && coordY > 0";
+        }
+    }
+    return diapozon;
+}
+
+System.Console.WriteLine( "Введите номер четверти: ");
+int numberArea = Convert.ToInt32(Console.ReadLine());
+
+string diapozon = DiapozonCoords(numberArea);
+System.Console.WriteLine(diapozon);
+             

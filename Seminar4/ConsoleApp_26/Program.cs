10b62e3a12d5deb817cf7a41b92a3ae5ea1a1497
Aleksey <raven.lexa@yandex.ru>
2022-11-09 21:03:46 +0300

    ConsoleApp_26

---
+++ Seminar4/ConsoleApp_26/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе.*/
+int LengthNumber(int number)
+{
+    string strNumber = Math.Abs(number).ToString();
+    return strNumber.Length;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = LengthNumber(number);
+System.Console.WriteLine(sum);

19b845d92d7d76b22576015020b19cd0305d988e
Aleksey <raven.lexa@yandex.ru>
2022-11-09 20:26:33 +0300

    ConsoleApp_24

---
+++ Seminar4/ConsoleApp_24/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число (А) 
+и выдаёт сумму чисел от 1 до А.*/
+int GetSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = GetSum(number);
+Console.WriteLine(sum);

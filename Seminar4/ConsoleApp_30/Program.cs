9c213b755e5fdc1a446863b36f8ac0c614bdcdcc
Aleksey <raven.lexa@yandex.ru>
2022-11-09 22:59:53 +0300

    ConsoleApp_30

---
+++ Seminar4/ConsoleApp_30/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая выводит массив из 8 элементов, заполненный нулями 
+и единицами в случайном порядке.*/
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+}

beebd4c1b05694d8fcdcbff68316a212676dafa8
Aleksey <raven.lexa@yandex.ru>
2022-11-12 13:48:52 +0300

    ConsoleApp_33

---
+++ Seminar5/ConsoleApp_33/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задайте массив. Напишите программу, которая определяет, 
+присутствует ли заданное число в массиве.*/
+bool Search(int[] array, int Num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == Num)
+        {
+            return true;   
+        }
+    }
+    return false;       
+
+}
+
+
+
+int[] array = new int[9] {12, 23, -45, 6, -47, 3, 82, -73, -6};
+System.Console.WriteLine("Введите искомое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool SearchNum = Search(array, num);
+System.Console.WriteLine(SearchNum);
+

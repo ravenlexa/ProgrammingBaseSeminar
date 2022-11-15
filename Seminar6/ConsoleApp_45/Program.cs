21da78b5eae6f22cf2edaad7b7676c9c42e33f14
Aleksey <raven.lexa@yandex.ru>
2022-11-15 13:31:33 +0300

    ConsoleApp_45

---
+++ Seminar6/ConsoleApp_45/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая будет создавать копию 
+заданного массива с помощью поэлементного копирования.*/
+void PrintArray(int[] array)
+{
+    Console.Write(" Копия: ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+int[] array = new int [11];
+Console.Write("Массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    System.Console.Write(array[i] + ", ");
+}
+
+for (int i = 0; i < array.Length -1; i++)
+{
+    int temp = array[i];
+    //array[i] = array[(array.Length - 1) - i];
+    //array[(array.Length - 1) - i] = temp;
+}
+System.Console.WriteLine();
+PrintArray(array);
+

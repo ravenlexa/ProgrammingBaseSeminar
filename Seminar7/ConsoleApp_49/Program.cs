fcfb06767bd3d4ad05186818aa9d3ddb0bb0975b
Aleksey <raven.lexa@yandex.ru>
2022-11-21 19:52:41 +0300

    ConsoleApp_49

---
+++ Seminar7/ConsoleApp_49/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.*/
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Console.WriteLine();
+FillArray(array);
+
+for (int i = 0; i < array.GetLength(0); i = i + 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j = j + 2)
+        {
+            array[i, j] = array[i, j] * array[i, j];
+        }
+        Console.WriteLine();
+    }
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }    
+
+

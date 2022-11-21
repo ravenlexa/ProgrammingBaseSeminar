dd01810e4ffc44735b235eb3b62ea9eee57a4edc
Aleksey <raven.lexa@yandex.ru>
2022-11-21 19:28:07 +0300

    ConsoleApp_48

---
+++ Seminar7/ConsoleApp_48/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.*/
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = i + j;
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}

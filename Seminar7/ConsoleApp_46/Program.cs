6ed35c28e2b7d8ee71cac45735d2eca2526d2bea
Aleksey <raven.lexa@yandex.ru>
2022-11-21 19:07:42 +0300

    ConsoleApp_46

---
+++ Seminar7/ConsoleApp_46/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте двумерный массив размером m×n, 
+заполненный случайными целыми числами.*/
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(1, 100);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}

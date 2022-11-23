f7df39a391c8b60c01dad0d7d550a2dd14c25998
Aleksey <raven.lexa@yandex.ru>
2022-11-23 19:23:39 +0300

    ConsoleApp_53

---
+++ Seminar8/ConsoleApp_53/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая поменяет 
+местами первую и последнюю строку массива.*/
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write(array[i, j] + " ");
+        }    
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                int temp = array[i, j];
+                array[i, j] = array[array.GetLength(0) - 1, j];
+                array[array.GetLength(0) - 1, j] = temp;
+            }
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter Line: ");
+int Line = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Column: ");
+int Column = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[Line, Column];
+
+FillArray(array);
+Console.WriteLine();
+ChangeArray(array);
